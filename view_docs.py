1135ea5bd98ad069ec428de2cbc1d08ebe0a0ad1
Maxwell G. Brown <mbrown@morpace.com>
2015-06-24 16:02:38 -0400

    Initial commit

---
+++ view_docs.py
@@ -0,0 +1,9 @@
+﻿import os
+
+# build the sphinx docs before running
+# fabric/docs/buildpath
+os.system("cd ./docs && make html")
+
+# run sphinx with xdg
+# fabric/docs/local_path
+os.system("xdg-open ./docs/_build/html/index.html 2>&1 > /dev/null &")
