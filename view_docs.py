e66e41b42663c367931a57e9c7371ea18587a014
Maxwell G. Brown <mbrown@morpace.com>
2016-03-09 10:37:12 -0500

    Added a --refresh flag to view_docs.py

--- view_docs.py
+++ view_docs.py
@@ -1,21 +1,33 @@
-﻿import os
+﻿import argparse
+import os
 
 
 ROOT_DIR = os.path.dirname(__file__)
 
 
 def build_docs():
-    # build the sphinx docs before running
+    print("Builiding Notebook...\n")
     makefile_path = os.path.join(ROOT_DIR, "docs/")
     os.system("cd {} && make html".format(makefile_path))
 
 
 def view_docs():
-    # run sphinx with xdg on built files
+    print("Opening Notebook in web browser...")
     index_path = os.path.join(ROOT_DIR, "docs/build/html/index.html")
     os.system("xdg-open {} 2>&1 > /dev/null &".format(index_path))
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Open Notebook in Web Browser")
+    parser.add_argument('--refresh', '-r',
+            action="store_true", dest="refresh", default=False,
+            help="Rebuild Notebook but do not launch xdg-open again.")
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
+    args = parse_args()
+
     build_docs()
-    view_docs()
+    if args.refresh is not True:
+        view_docs()
