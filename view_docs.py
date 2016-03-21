14954015ca2fa6570cbb74374fb825eed4030c61
Maxwell G. Brown <mbrown@morpace.com>
2016-03-21 11:11:37 -0400

    added --rebuild to view_docs.py

--- view_docs.py
+++ view_docs.py
@@ -1,8 +1,16 @@
 ﻿import argparse
 import os
+import shutil
 
 
 ROOT_DIR = os.path.dirname(__file__)
+
+
+def destroy_build():
+    """Destroys build so make gives full sphinx build errors"""
+    print("Destroying previous build...\n")
+    build_path = os.path.join(ROOT_DIR, "docs", "build")
+    shutil.rmtree(build_path)
 
 
 def build_docs():
@@ -21,13 +29,18 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Open Notebook in Web Browser")
     parser.add_argument('--refresh', '-r',
             action="store_true", dest="refresh", default=False,
-            help="Rebuild Notebook but do not launch xdg-open again.")
+            help="Run sphinx-build but do not launch xdg-open again.")
+    parser.add_argument('--rebuild', '-d',
+            action='store_true', dest="rebuild", default=False,
+            help="Remove previous build before running sphinx-build.")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
 
+    if args.rebuild is True:
+        destroy_build()
     build_docs()
     if args.refresh is not True:
         view_docs()
