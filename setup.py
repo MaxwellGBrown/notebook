075d0b5d87ae1e590547f4c3fb1fcd2ff9078868
Maxwell G. Brown <mbrown@morpace.com>
2016-09-01 10:06:12 -0400

    made a basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+﻿from setuptools import setup
+
+
+setup_kwargs = {
+        "name": "notebook",
+        "version": "0.1",
+        "url": "https://github.com/MaxwellGBrown",
+        }
+
+
+if __name__ == "__main__":
+    setup(**setup_kwargs)
