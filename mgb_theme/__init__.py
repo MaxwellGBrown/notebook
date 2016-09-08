41f43cf67f564242f3b2ad1ed5d02651ad0b8276
Maxwell G. Brown <mbrown@morpace.com>
2016-09-08 16:18:04 -0400

    turned mgb_theme into an extension also

--- mgb_theme/__init__.py
+++ mgb_theme/__init__.py
@@ -1,20 +1,16 @@
 ﻿import os
 
-from alabaster import _version as version
-
 
 def get_path():
-    """
-    Shortcut for users whose theme is next to their conf.py.
-    """
+    """ Entrypoint for setup.py """
     # Theme directory is defined as our parent directory
     return os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 
 def update_context(app, pagename, templatename, context, doctree):
-    context['alabaster_version'] = version.__version__
+    context['mgb_theme'] = True
+
 
 def setup(app):
     app.connect('html-page-context', update_context)
-    return {'version': version.__version__,
-            'parallel_read_safe': True}
+    return {'version': "0.0.0"}
