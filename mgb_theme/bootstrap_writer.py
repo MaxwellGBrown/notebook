7351ebff31c94a2352cc364c35a47c88080c6678
Maxwell G. Brown <mbrown@morpace.com>
2016-09-08 16:39:00 -0400

    outlined bootstrap_writer now that mgb_theme is an extension

---
+++ mgb_theme/bootstrap_writer.py
@@ -0,0 +1,19 @@
+﻿"""
+bootstrap_writer.py
+
+While there are a few projects for turning .rst into bootstrap html, I want to
+have custom control over how my application does it.
+
+bootstrap-rst
+-------------
+https://github.com/rougier/bootstrap-rst
+
+NOTE:
+    see this for how to interact w/ docutils writers!
+https://github.com/rougier/bootstrap-rst/blob/master/bootstrap.py
+
+
+basicstrap
+----------
+https://github.com/tell-k/sphinxjp.themes.basicstrap/tree/master/src
+"""
