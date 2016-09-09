da6f59783d36a676e0bf9d754a1e5d285d87ea99
Maxwell G. Brown <mbrown@morpace.com>
2016-09-09 11:36:38 -0400

    turned code blocks into bootstrap4 cards

--- mgb_theme/bootstrap_writer.py
+++ mgb_theme/bootstrap_writer.py
@@ -25,6 +25,7 @@ from docutils.writers.html4css1 import HTMLTranslator as docutils_HTMLTranslator
 from docutils import nodes
 
 from sphinx.writers.html import HTMLTranslator as sphinx_HTMLTranslator
+from sphinx.locale import _
 
 
 class BootstrapTranslator(sphinx_HTMLTranslator):
@@ -45,9 +46,61 @@ class BootstrapTranslator(sphinx_HTMLTranslator):
         return docutils_HTMLTranslator.depart_literal(self, node)
 
     def visit_literal_block(self, node):
-        # docutils uses <code></code> instead of a bunch of junk like sphinx
-        return docutils_HTMLTranslator.visit_literal_block(self, node)
+        parent_is_container = isinstance(node.parent, nodes.container)
+        if parent_is_container is False:
+            card = self.starttag(dict(), "div", "", CLASS='card')
+            self.body.append(card)
+
+        self.body.append(self.starttag(dict(), 'div', CLASS='card-block'))
+        self.body.append(self.starttag(node, 'pre', CLASS='literal-block'))
 
     def depart_literal_block(self, node):
-        # docutils uses <code></code> instead of a bunch of junk like sphinx
-        return docutils_HTMLTranslator.depart_literal_block(self, node)
+        self.body.append("</pre>\n")
+        self.body.append("</div>\n")
+
+        parent_is_container = isinstance(node.parent, nodes.container)
+        if parent_is_container is False:
+            self.body.append("</div>\n")  # terminate card
+
+    def visit_container(self, node):
+        """ Add .card to all containers making them bootstrap cards """
+        self.body.append(self.starttag(node, 'div', CLASS='card'))
+
+    def depart_container(self, node):
+        self.body.append('</div>\n')
+
+    def visit_caption(self, node):
+        parent_is_container = isinstance(node.parent, nodes.container)
+        if parent_is_container is True:
+            card_header = self.starttag(node, 'div', '', CLASS='card-header')
+            self.body.append(card_header)
+
+        if parent_is_container and node.parent.get('literal_block'):
+            self.body.append('<div class="code-block-caption">')
+        else:
+            docutils_HTMLTranslator.visit_caption(self, node)
+        self.add_fignumber(node.parent)
+
+    def depart_caption(self, node):
+        # self.body.append("</div>\n")
+
+        # append permalink if available
+        if isinstance(node.parent, nodes.container) and \
+                node.parent.get('literal_block'):
+            self.add_permalink_ref(node.parent, _('Permalink to this code'))
+        elif isinstance(node.parent, nodes.figure):
+            image_nodes = node.parent.traverse(nodes.image)
+            target_node = image_nodes and image_nodes[0] or node.parent
+            self.add_permalink_ref(target_node, _('Permalink to this image'))
+        elif node.parent.get('toctree'):
+            self.add_permalink_ref(node.parent.parent,
+                    _('Permalink to this toctree'))
+
+        if isinstance(node.parent, nodes.container) and \
+                node.parent.get('literal_block'):
+            self.body.append('</div>\n')
+        else:
+            BaseTranslator.depart_caption(self, node)
+
+        self.body.append('</div>\n')
+
