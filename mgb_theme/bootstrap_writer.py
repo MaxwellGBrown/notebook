55d3e547fa1e4688e14d187d25092f40162877ca
Maxwell G. Brown <mbrown@morpace.com>
2016-09-09 10:57:47 -0400

    using <code></code> instead of <pre><div></div></pre>

--- mgb_theme/bootstrap_writer.py
+++ mgb_theme/bootstrap_writer.py
@@ -20,3 +20,34 @@ https://github.com/tell-k/sphinxjp.themes.basicstrap/tree/master/src
 
 # To use a custom writer, you'll also probably have to define a special builder
 # class that specifically uses that builder.
+
+from docutils.writers.html4css1 import HTMLTranslator as docutils_HTMLTranslator
+from docutils import nodes
+
+from sphinx.writers.html import HTMLTranslator as sphinx_HTMLTranslator
+
+
+class BootstrapTranslator(sphinx_HTMLTranslator):
+    """
+    Inherits from sphinx.writers.html.HTMLTranslator which extends docutils'
+    docutils.writers.html4css1.HTMLTranslator
+    """
+
+    def __init__(self, *args, **kwargs):
+        sphinx_HTMLTranslator.__init__(self, *args, **kwargs)
+
+    def visit_literal(self, node):
+        # docutils uses <code></code> instead of a bunch of junk like sphinx
+        return docutils_HTMLTranslator.visit_literal(self, node)
+
+    def depart_literal(self, node):
+        # docutils uses <code></code> instead of a bunch of junk like sphinx
+        return docutils_HTMLTranslator.depart_literal(self, node)
+
+    def visit_literal_block(self, node):
+        # docutils uses <code></code> instead of a bunch of junk like sphinx
+        return docutils_HTMLTranslator.visit_literal_block(self, node)
+
+    def depart_literal_block(self, node):
+        # docutils uses <code></code> instead of a bunch of junk like sphinx
+        return docutils_HTMLTranslator.depart_literal_block(self, node)
